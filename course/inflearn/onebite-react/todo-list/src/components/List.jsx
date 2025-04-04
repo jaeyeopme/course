302d64a251c07086cfeff8cb6432d6b2b725eb52
jaeyeopme <jaeyeopme@gmail.com>
2025-04-04 23:31:55 +0900

    add todo statistics display using useMemo for performance optimization

--- course/inflearn/onebite-react/todo-list/src/components/List.jsx
+++ course/inflearn/onebite-react/todo-list/src/components/List.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useMemo, useState } from 'react'
 import './List.css'
 import TodoItem from './TodoItem'
 
@@ -17,9 +17,20 @@ const List = ({ todos, onUpdate, onDelete }) => {
         )
   }
 
+  const { totalCount, doneCount, notDoneCount } = useMemo(() => {
+    const totalCount = todos.length
+    const doneCount = todos.filter((todo) => todo.isDone).length
+    return { totalCount, doneCount, notDoneCount: totalCount - doneCount }
+  }, [todos])
+
   return (
     <div className='List'>
       <h4>Todo List 🌱</h4>
+      <div>
+        <div>total: {totalCount}</div>
+        <div>done: {doneCount}</div>
+        <div>not done: {notDoneCount}</div>
+      </div>
       <input
         type='text'
         placeholder='검색어를 입력하세요.'
