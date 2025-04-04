e12923ad2923c983ca080e86202be735d670a08c
jaeyeopme <jaeyeopme@gmail.com>
2025-04-05 01:05:10 +0900

    optimize rendering of Header and TodoItem components using React.memo

--- course/inflearn/onebite-react/todo-list/src/components/TodoItem.jsx
+++ course/inflearn/onebite-react/todo-list/src/components/TodoItem.jsx
@@ -1,3 +1,4 @@
+import { memo } from 'react'
 import './TodoItem.css'
 
 const TodoItem = ({ id, isDone, content, date, onUpdate, onDelete }) => {
@@ -11,4 +12,14 @@ const TodoItem = ({ id, isDone, content, date, onUpdate, onDelete }) => {
   )
 }
 
-export default TodoItem
+// 고차 컴포넌트(HOC): 인자로 넘긴 컴포넌트에게 로직을 추가하여 엘리먼트를 반환하게 됨.
+// React.memo 는 컴포넌트의 props 가 변경되지 않는 한 리렌더링을 방지함.
+// 함수들은 App 컴포넌트가 재생성되면 주소 값이 바뀌어 변경된 것으로 인식하기 때문에 props 변경감지를 위한 콜백함수에서 명시적으로 제외.
+export default memo(
+  TodoItem,
+  (prevProps, nextProps) =>
+    prevProps.id === nextProps.id &&
+    prevProps.isDone === nextProps.isDone &&
+    prevProps.content === nextProps.content &&
+    prevProps.date === nextProps.date
+)
