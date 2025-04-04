09196babf3ad861e4b459b28115e2b5f7a8bd98d
jaeyeopme <jaeyeopme@gmail.com>
2025-04-04 22:21:52 +0900

    refactor App component to use useReducer for state management of todos

--- course/inflearn/onebite-react/todo-list/src/App.jsx
+++ course/inflearn/onebite-react/todo-list/src/App.jsx
@@ -1,52 +1,61 @@
-import { useRef, useState } from 'react'
+import { useReducer, useRef } from 'react'
 import './App.css'
 import Editor from './components/Editor'
 import Header from './components/Header'
 import List from './components/List'
 
+const mockData = [
+  {
+    id: 0,
+    isDone: false,
+    content: 'Learn React',
+    date: new Date().getTime(),
+  },
+  {
+    id: 1,
+    isDone: false,
+    content: 'Learn Typescript',
+    date: new Date().getTime(),
+  },
+  {
+    id: 2,
+    isDone: false,
+    content: 'Learn Next',
+    date: new Date().getTime(),
+  },
+]
+
+function reducer(todos, action) {
+  switch (action.type) {
+    case 'CREATE':
+      return [action.data, ...todos]
+    case 'UPDATE':
+      return todos.map((todo) =>
+        todo.id === action.id ? { ...todo, isDone: !todo.isDone } : todo
+      )
+    case 'DELETE':
+      return todos.filter((todo) => todo.id !== action.id)
+    default:
+      return todos
+  }
+}
+
 function App() {
-  const mockData = [
-    {
-      id: 0,
-      isDone: false,
-      content: 'Learn React',
-      date: new Date().getTime(),
-    },
-    {
-      id: 1,
-      isDone: false,
-      content: 'Learn Typescript',
-      date: new Date().getTime(),
-    },
-    {
-      id: 2,
-      isDone: false,
-      content: 'Learn Next',
-      date: new Date().getTime(),
-    },
-  ]
-  const [todos, setTodos] = useState(mockData)
+  const [todos, dispatch] = useReducer(reducer, mockData)
   const idRef = useRef(3)
 
-  const onCreate = (content) => {
-    const createdTodo = {
-      id: idRef.current++,
-      isDone: false,
-      content: content,
-      date: new Date().getTime(),
-    }
-    setTodos([createdTodo, ...todos])
-  }
-
-  const onUpdate = (id) => {
-    setTodos(
-      todos.map((todo) =>
-        todo.id === id ? { ...todo, isDone: !todo.isDone } : todo
-      )
-    )
-  }
-
-  const onDelete = (id) => setTodos(todos.filter((todo) => todo.id !== id))
+  const onCreate = (content) =>
+    dispatch({
+      type: 'CREATE',
+      data: {
+        id: idRef.current++,
+        isDone: false,
+        content: content,
+        date: new Date().getTime(),
+      },
+    })
+  const onUpdate = (id) => dispatch({ type: 'UPDATE', id })
+  const onDelete = (id) => dispatch({ type: 'DELETE', id })
 
   return (
     <div className='App'>
