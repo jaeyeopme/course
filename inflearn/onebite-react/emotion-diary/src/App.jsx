fc2ccb3ba1bbd99e8de62aecdd3d8e3d04dd4bd1
jaeyeopme <jaeyeopme@gmail.com>
2025-04-06 18:45:20 +0900

    refactor App component to implement context for state management; add CRUD operations for diary entries

--- inflearn/onebite-react/emotion-diary/src/App.jsx
+++ inflearn/onebite-react/emotion-diary/src/App.jsx
@@ -1,4 +1,4 @@
-import { useReducer } from 'react'
+import { createContext, useReducer, useRef } from 'react'
 import { Route, Routes } from 'react-router-dom'
 import './App.css'
 import Button from './components/Button'
@@ -11,10 +11,10 @@ import NotFound from './pages/NotFound'
 
 const mockData = [
   {
-    id: 1,
+    id: 3,
     createDate: new Date().getTime(),
     emotionId: 1,
-    content: '1번 일기',
+    content: '3번 일기',
   },
   {
     id: 2,
@@ -22,20 +22,68 @@ const mockData = [
     emotionId: 1,
     content: '2번 일기',
   },
+
   {
-    id: 3,
+    id: 1,
     createDate: new Date().getTime(),
     emotionId: 1,
-    content: '3번 일기',
+    content: '1번 일기',
   },
 ]
 
 function reducer(state, action) {
-  return state
+  switch (action.type) {
+    case 'CREATE':
+      return [action.data, ...state]
+    case 'UPDATE':
+      return state.map((it) =>
+        // PUT
+        String(it.id) === String(action.data.id) ? action.data : it
+      )
+    case 'DELETE':
+      return state.filter((it) => String(it.id) !== String(action.id))
+    default:
+      return state
+  }
 }
+
+const DiaryStateContext = createContext()
+const DiaryDispatchContext = createContext()
 
 function App() {
   const [data, dispatch] = useReducer(reducer, mockData)
+  const idRef = useRef(4)
+
+  const onCreate = (createDate, emotionId, content) => {
+    dispatch({
+      type: 'CREATE',
+      data: {
+        id: idRef.current++,
+        createDate,
+        emotionId,
+        content,
+      },
+    })
+  }
+
+  const onUpdate = (id, createDate, emotionId, content) => {
+    dispatch({
+      type: 'UPDATE',
+      data: {
+        id,
+        createDate,
+        emotionId,
+        content,
+      },
+    })
+  }
+
+  const onDelete = (id) => {
+    dispatch({
+      type: 'DELETE',
+      id,
+    })
+  }
 
   return (
     <>
@@ -44,13 +92,17 @@ function App() {
         left_child={<Button text={'left'} />}
         right_child={<Button text={'right'} />}
       />
-      <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/new' element={<New />} />
-        <Route path='/diary/:id' element={<Diary />} />
-        <Route path='/edit/:id' element={<Edit />} />
-        <Route path='*' element={<NotFound />} />
-      </Routes>
+      <DiaryStateContext.Provider value={data}>
+        <DiaryDispatchContext.Provider value={{ onCreate, onUpdate, onDelete }}>
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/new' element={<New />} />
+            <Route path='/diary/:id' element={<Diary />} />
+            <Route path='/edit/:id' element={<Edit />} />
+            <Route path='*' element={<NotFound />} />
+          </Routes>
+        </DiaryDispatchContext.Provider>
+      </DiaryStateContext.Provider>
     </>
   )
 }
