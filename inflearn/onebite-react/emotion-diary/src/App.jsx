38fb751a9cd74c73fed7905690325414ee3d0d82
jaeyeopme <jaeyeopme@gmail.com>
2025-04-06 18:16:11 +0900

    add Edit page and integrate with routing; implement mock data and useReducer for state management

--- inflearn/onebite-react/emotion-diary/src/App.jsx
+++ inflearn/onebite-react/emotion-diary/src/App.jsx
@@ -1,13 +1,42 @@
+import { useReducer } from 'react'
 import { Route, Routes } from 'react-router-dom'
 import './App.css'
 import Button from './components/Button'
 import Header from './components/Header'
 import Diary from './pages/Diary'
+import Edit from './pages/Edit'
 import Home from './pages/Home'
 import New from './pages/New'
 import NotFound from './pages/NotFound'
 
+const mockData = [
+  {
+    id: 1,
+    createDate: new Date().getTime(),
+    emotionId: 1,
+    content: '1번 일기',
+  },
+  {
+    id: 2,
+    createDate: new Date().getTime(),
+    emotionId: 1,
+    content: '2번 일기',
+  },
+  {
+    id: 3,
+    createDate: new Date().getTime(),
+    emotionId: 1,
+    content: '3번 일기',
+  },
+]
+
+function reducer(state, action) {
+  return state
+}
+
 function App() {
+  const [data, dispatch] = useReducer(reducer, mockData)
+
   return (
     <>
       <Header
@@ -19,6 +48,7 @@ function App() {
         <Route path='/' element={<Home />} />
         <Route path='/new' element={<New />} />
         <Route path='/diary/:id' element={<Diary />} />
+        <Route path='/edit/:id' element={<Edit />} />
         <Route path='*' element={<NotFound />} />
       </Routes>
     </>
