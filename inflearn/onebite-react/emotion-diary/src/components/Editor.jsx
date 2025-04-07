a6bf8b3196a5468784d6cd1d22ceb253d5c208c5
jaeyeopme <jaeyeopme@gmail.com>
2025-04-07 17:01:37 +0900

    add Editor and EmotionItem components; implement emotion selection and date input; enhance New page layout

---
+++ inflearn/onebite-react/emotion-diary/src/components/Editor.jsx
@@ -0,0 +1,54 @@
+import './Editor.css'
+import EmotionItem from './EmotionItem'
+
+const emotionList = [
+  {
+    id: 1,
+    name: '완전 좋음',
+  },
+  {
+    id: 2,
+    name: '좋음',
+  },
+  {
+    id: 3,
+    name: '그럭저럭',
+  },
+  {
+    id: 4,
+    name: '나쁨',
+  },
+  {
+    id: 5,
+    name: '끔찍함',
+  },
+]
+
+const Editor = () => {
+  const emotionId = 1
+
+  return (
+    <div className='Editor'>
+      <section className='date_section'>
+        <h4>오늘의 날짜</h4>
+        <input type='date' value={new Date()} />
+      </section>
+      <section className='emotion_section'>
+        <h4>오늘의 감정</h4>
+        <div className='emotion_list_wrapper'>
+          {emotionList.map((emotion) => (
+            <EmotionItem
+              key={emotion.id}
+              {...emotion}
+              isSelected={emotion.id === emotionId}
+            />
+          ))}
+        </div>
+      </section>
+      <section className='content_section'></section>
+      <section className='button_section'></section>
+    </div>
+  )
+}
+
+export default Editor
