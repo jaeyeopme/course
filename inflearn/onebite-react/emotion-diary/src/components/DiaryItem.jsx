d53271cfa8928c1f552b2096ee67d9a17bbc860a
jaeyeopme <jaeyeopme@gmail.com>
2025-04-07 16:18:56 +0900

    refactor diary components to use createdDate instead of createDate; implement sorting in DiaryList; enhance Home page with monthly data filtering

--- inflearn/onebite-react/emotion-diary/src/components/DiaryItem.jsx
+++ inflearn/onebite-react/emotion-diary/src/components/DiaryItem.jsx
@@ -1,23 +1,27 @@
+import { useNavigate } from 'react-router-dom'
 import { getEmotionImage } from '../util/get-emotion-image'
 import Button from './Button'
 import './DiaryItem.css'
 
-const DiaryItem = () => {
-  const emotionId = 2
+const DiaryItem = ({ id, createdDate, emotionId, content }) => {
+  const navigate = useNavigate()
 
   return (
     <div className='DiaryItem'>
-      <div className={`img_section img_section_${emotionId}`}>
+      <div
+        onClick={() => navigate(`/diary/${id}`)}
+        className={`img_section img_section_${emotionId}`}
+      >
         <img src={getEmotionImage(emotionId)} />
       </div>
-      <div className='info_section'>
+      <div onClick={() => navigate(`/diary/${id}`)} className='info_section'>
         <div className='created_date'>
-          {new Date().toLocaleDateString('ko')}
+          {createdDate.toLocaleDateString('ko')}
         </div>
-        <div className='content'>content</div>
+        <div className='content'>{content}</div>
       </div>
       <div className='button_section'>
-        <Button text={'수정하기'} />
+        <Button onClick={() => navigate(`/edit/${id}`)} text={'수정하기'} />
       </div>
     </div>
   )
--- inflearn/onebite-react/emotion-diary/src/components/DiaryList.jsx
+++ inflearn/onebite-react/emotion-diary/src/components/DiaryList.jsx
@@ -1,19 +1,41 @@
+import { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
 import Button from './Button'
 import DiaryItem from './DiaryItem'
 import './DiaryList.css'
 
-const DiaryList = () => {
+const getSortedData = (data, sortType) => {
+  return data.toSorted((a, b) => {
+    if (sortType === 'oldest') {
+      return a.createdDate - b.createdDate
+    }
+    return b.createdDate - a.createdDate
+  })
+}
+
+const DiaryList = ({ data }) => {
+  const navigate = useNavigate()
+  const [sortType, setSortType] = useState('latest')
+
+  const onChangeSortType = (e) => setSortType(e.target.value)
+
   return (
     <div className='DiaryList'>
       <div className='menu_bar'>
-        <select>
+        <select onChange={onChangeSortType}>
           <option value={'latest'}>최신순</option>
           <option value={'oldest'}>오래된순</option>
         </select>
-        <Button text={'새 일기 쓰기'} type={'POSITIVE'} />
+        <Button
+          onClick={() => navigate('/new')}
+          text={'새 일기 쓰기'}
+          type={'POSITIVE'}
+        />
       </div>
       <div className='list_wrapper'>
-        <DiaryItem />
+        {getSortedData(data, sortType).map((it) => (
+          <DiaryItem key={it.id} {...it} />
+        ))}
       </div>
     </div>
   )
