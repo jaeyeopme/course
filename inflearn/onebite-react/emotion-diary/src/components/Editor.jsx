fce2d0be2052eefc9752df821ac99e3358d103ea
jaeyeopme <jaeyeopme@gmail.com>
2025-04-07 18:28:10 +0900

    enhance Editor and EmotionItem components: refactor input handling and integrate onSubmit for diary creation

--- inflearn/onebite-react/emotion-diary/src/components/Editor.jsx
+++ inflearn/onebite-react/emotion-diary/src/components/Editor.jsx
@@ -1,3 +1,5 @@
+import { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
 import Button from './Button'
 import './Editor.css'
 import EmotionItem from './EmotionItem'
@@ -25,34 +27,69 @@ const emotionList = [
   },
 ]
 
-const Editor = () => {
-  const emotionId = 1
+const Editor = ({ onSubmit }) => {
+  const navigate = useNavigate()
+  const [input, setInput] = useState({
+    createdDate: new Date(),
+    emotionId: 1,
+    content: '',
+  })
+
+  const onChangeInput = (e) => {
+    if (e.target.name === 'createdDate') {
+      setInput({ ...input, createdDate: new Date(e.target.value) })
+      return
+    }
+    setInput({ ...input, [e.target.name]: e.target.value })
+  }
 
   return (
     <div className='Editor'>
       <section className='date_section'>
         <h4>오늘의 날짜</h4>
-        <input type='date' value={new Date()} />
+        <input
+          name={'createdDate'}
+          onChange={onChangeInput}
+          type='date'
+          value={input.createdDate.toISOString().split('T')[0]}
+        />
       </section>
       <section className='emotion_section'>
         <h4>오늘의 감정</h4>
         <div className='emotion_list_wrapper'>
           {emotionList.map((emotion) => (
             <EmotionItem
+              name={'emotionId'}
+              onClick={() =>
+                onChangeInput({
+                  target: { name: 'emotionId', value: emotion.id },
+                })
+              }
               key={emotion.id}
               {...emotion}
-              isSelected={emotion.id === emotionId}
+              isSelected={emotion.id === input.emotionId}
             />
           ))}
         </div>
       </section>
       <section className='content_section'>
         <h4>오늘의 일기</h4>
-        <textarea placeholder='오늘은 어땠나요?'></textarea>
+        <textarea
+          onChange={onChangeInput}
+          name={'content'}
+          placeholder='오늘은 어땠나요?'
+        ></textarea>
       </section>
       <section className='button_section'>
-        <Button text={'취소하기'} />
-        <Button text={'작성완료'} type={'POSITIVE'} />
+        <Button text={'취소하기'} onClick={() => navigate(-1)} />
+        <Button
+          text={'작성완료'}
+          type={'POSITIVE'}
+          onClick={() => {
+            onSubmit(input)
+            navigate('/', { replace: true })
+          }}
+        />
       </section>
     </div>
   )
