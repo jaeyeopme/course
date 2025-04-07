eace61ece519287dc7cd2bd08a95fd55b6090a79
jaeyeopme <jaeyeopme@gmail.com>
2025-04-07 22:47:20 +0900

    enhance Viewer and Diary components: pass diary data to Viewer for dynamic content display

--- inflearn/onebite-react/emotion-diary/src/pages/Diary.jsx
+++ inflearn/onebite-react/emotion-diary/src/pages/Diary.jsx
@@ -2,15 +2,24 @@ import { useNavigate, useParams } from 'react-router-dom'
 import Button from '../components/Button'
 import Header from '../components/Header'
 import Viewer from '../components/Viewer'
+import useDiary from '../components/useDiary'
+import { getDate } from '../util/get-date'
 
 const Diary = () => {
   const navigate = useNavigate()
   const params = useParams()
+  const diary = useDiary(params.id)
+
+  if (!diary) {
+    return <div>로딩중입니다...</div>
+  }
+
+  const { createdDate, emotionId, content } = diary
 
   return (
     <div>
       <Header
-        title={'다이어리'}
+        title={`${getDate(createdDate)} 기록`}
         left_child={<Button onClick={() => navigate(-1)} text={'뒤로가기'} />}
         right_child={
           <Button
@@ -19,7 +28,7 @@ const Diary = () => {
           />
         }
       />
-      <Viewer />
+      <Viewer emotionId={emotionId} content={content} />
     </div>
   )
 }
