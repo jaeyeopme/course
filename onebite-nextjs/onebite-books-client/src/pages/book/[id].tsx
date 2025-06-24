195f949bbbf57c5fc31ac657627d9f3fe61eafc0
jaeyeopme <jaeyeopme@gmail.com>
2025-06-24 18:07:23 +0900

    fix comment: dynamic routing 설명 개선

--- onebite-nextjs/onebite-books-client/src/pages/book/[id].tsx
+++ onebite-nextjs/onebite-books-client/src/pages/book/[id].tsx
@@ -1,25 +1,21 @@
 import { useRouter } from "next/router";
 
-
 /**
  * Dynamic Routing
  *
  * 1. Dynamic Segment: [id].tsx
  *    - 파일 이름에 대괄호를 사용하여 동적 경로를 생성
- *    - URL에서 'id' 부분을 동적으로 받아와서 페이지를 렌더링
+ *    - 세그먼트가 제공되지 않으면 404 페이지로 이동
  *    - 예시: /posts/123 → id = "123"
  *
  * 2. Catch All Segment: [...id].tsx
  *    - 파일 이름에 대괄호와 rest 문법을 사용하여 모든 하위 경로를 캐치
- *    - URL에서 '/'로 구분된 모든 경로의 값들을 배열 형태로 받아옴
+ *    - 세그먼트가 제공되지 않으면 404 페이지로 이동
  *    - 예시: /posts/a/b/c → id = ["a", "b", "c"]
- *    - 경로 값이 없으면 404 페이지로 이동
  *
  * 3. Optional Catch All Segment: [[...id]].tsx
- *    - 파일 이름에 이중 대괄호와 rest 문법을 사용
- *    - Catch All과 동일하지만 경로 값이 없을 경우에도 페이지가 렌더링됨
+ *    - Catch All Segment 와 동일하게 동작하지만, 세그먼트가 제공되지 않아도 페이지가 렌더링됨
  *    - 예시: /posts → id = []
- *    - 예시: /posts/a/b → id = ["a", "b"]
  */
 export default function Page() {
   const router = useRouter();
