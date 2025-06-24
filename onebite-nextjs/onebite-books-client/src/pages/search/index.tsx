1a56c59cf71bf0cd12e9e95a0a722667a822b79d
jaeyeopme <jaeyeopme@gmail.com>
2025-06-25 00:30:39 +0900

    study: prefetching 학습

--- onebite-nextjs/onebite-books-client/src/pages/search/index.tsx
+++ onebite-nextjs/onebite-books-client/src/pages/search/index.tsx
@@ -1,5 +1,6 @@
 import Link from "next/link";
 import { useRouter } from "next/router";
+import { useEffect } from "react";
 
 /**
  * Query Parameter
@@ -22,7 +23,7 @@ import { useRouter } from "next/router";
  * 2. router.push()를 사용한 프로그래매틱 네비게이션
  *    - useRouter 훅의 push 메서드로 페이지를 이동
  *    - 버튼 클릭 등의 이벤트 핸들러에서 사용
- *    - 예시: router.push("/search?query=world")
+ *    - 예시: router.push("/book/15")
  */
 export default function Page() {
 	const router = useRouter();
@@ -30,13 +31,20 @@ export default function Page() {
 
 	const handleClick = () => {
 		// back, forward, replace 등 다양한 메서드 사용 가능
-		router.push("/search?query=world");
+		router.push("/book/15");
 	};
 
+	useEffect(() => {
+		router.prefetch("/book"); // 동적 라우팅은 프리페치를 수동으로 활성화해야 함
+	}, [router]);
+
+	// Link 컴포넌트는 기본적으로 프리페칭이 활성화되어있고, prefetch props 를 통해 비활성화 가능
 	return (
 		<>
 			<h1>Hello, {query}</h1>
-			<Link href="/search?query=nextjs">nextjs link</Link>
+			<Link prefetch={false} href="/search?query=nextjs">
+				nextjs link
+			</Link>
 			&nbsp;|&nbsp;
 			<button type="button" onClick={handleClick}>
 				world button
