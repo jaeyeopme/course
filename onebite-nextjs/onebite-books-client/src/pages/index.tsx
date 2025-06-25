6a0b416f3a7d077721d8f8626bd94a1a52a4fab5
jaeyeopme <jaeyeopme@gmail.com>
2025-06-25 19:27:53 +0900

    feat: index 페이지 UI 구현
    - book-item 컴포넌트 UI 구현
    - mock data 설정

--- onebite-nextjs/onebite-books-client/src/pages/index.tsx
+++ onebite-nextjs/onebite-books-client/src/pages/index.tsx
@@ -1,3 +1,23 @@
+import BookItem from "@/components/book-item";
+import books from "@/mock/books.json";
+import type { BookData } from "@/types";
+import style from "./index.module.css";
+
 export default function Home() {
-	return <h1>Home</h1>;
+	return (
+		<div className={style.container}>
+			<section>
+				<h3>지금 추천하는 도서</h3>
+				{books.map((book: BookData) => (
+					<BookItem key={book.id} {...book} />
+				))}
+			</section>
+			<section>
+				<h3>등록된 모든 도서</h3>
+				{books.map((book: BookData) => (
+					<BookItem key={book.id} {...book} />
+				))}
+			</section>
+		</div>
+	);
 }
