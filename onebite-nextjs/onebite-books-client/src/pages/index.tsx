75b4d2286c6fb196970b53fbb7f142bf207785b5
jaeyeopme <jaeyeopme@gmail.com>
2025-06-26 01:45:58 +0900

    study: SSR 학습
    - 서버 사이드 렌더링을 위해 예약된 함수명을 사용해야 함(getServerSideProps)
    - props 타입은 InferGetServerSidePropsType<typeof getServerSideProps> 를 통해 추론
    - 페이지 로드 시 컴포넌트 함수는 기본적으로 서버(Pre Render)에서 한 번, 클라이언트(Hydration)에서 한 번, 총 두 번 실행됨
    - 서버와 클라이언트는 서로 다른 실행 환경이므로 환경별 전용 객체에 접근할 때 주의해야 함
    - 환경별 전용 객체 유무로 환경을 구분하여 각 환경별 코드를 실행 가능
    - 서버 전용 데이터나 API 를 사용하기 위해서는 서버 사이드 렌더링 시 실행되는 함수인 getServerSideProps 를 사용
    - 클라이언트 전용 객체를 사용하기 위해서는 컴포넌트가 마운트된 후 실행되는 useEffect 를 사용

--- onebite-nextjs/onebite-books-client/src/pages/index.tsx
+++ onebite-nextjs/onebite-books-client/src/pages/index.tsx
@@ -1,12 +1,41 @@
-import type { ReactNode } from "react";
+import type { InferGetServerSidePropsType } from "next";
+import { type ReactNode, useEffect } from "react";
 import BookItem from "@/components/book-item";
 import SearchableLayout from "@/components/searchable-layout";
 import mock from "@/mock/books.json";
 import type { BookData } from "@/types";
-import style from "./index.module.css";
+import style from "./index.module.css"; //
 
-export default function Page() {
-	const books = mock as BookData[];
+// getServerSideProps: 서버 사이드 렌더링 시 매 요청마다 실행되는 예약된 함수명
+export const getServerSideProps = () => {
+	const data = mock as BookData[]; // 서버에서만 실행
+
+	return {
+		props: {
+			data,
+		},
+	};
+};
+
+// InferGetServerSidePropsType<typeof getServerSideProps> 로 타입을 추론
+export default function Page({
+	data: books,
+}: InferGetServerSidePropsType<typeof getServerSideProps>) {
+	// 서버(사전 렌더링)에서 한번 실행되고, 클라이언트(하이드레이션)에서 한번 더 실행됨
+	console.log("test"); // 서버와 클라이언트 모두에서 실행
+
+	// 환경별 전용 객체 유무로 환경을 구분하여 각 환경별 코드를 실행 가능
+	// window 객체는 클라이언트에서만 사용 가능하므로, 서버에서는 접근 불가
+	if (typeof window === "undefined") {
+		console.log("서버에서만 보이는 로그");
+	} else {
+		console.log("브라우저에서만 보이는 로그");
+	}
+
+	// 컴포넌트가 마운트된 후에 실행되므로, 클라이언트에서만 접근 가능한 객체 사용 가능
+	useEffect(() => {
+		console.log(window); // 클라이언트에서만 실행
+	}, []);
 
 	return (
 		<div className={style.container}>
