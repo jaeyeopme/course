6c31e657b4d896dbf20782f8f8d8077230411478
jaeyeopme <jaeyeopme@gmail.com>
2025-06-28 15:38:04 +0900

    feat: 인덱스 페이지 데이터 페칭 추가
    - 서버 URL을 public 환경 변수로 관리
    - 추천 도서 및 전체 도서 조회 컴포넌트 분리

--- onebite-nextjs/onebite-books-client-app-router/src/app/(with-searchbar)/page.tsx
+++ onebite-nextjs/onebite-books-client-app-router/src/app/(with-searchbar)/page.tsx
@@ -1,21 +1,57 @@
 import BookItem from "@/components/book-item";
-import books from "@/mock/books.json";
+import type { BookData } from "@/types";
 import style from "./page.module.css";
 
-export default function Home() {
+async function RecommendedBooks() {
+	const response = await fetch(
+		`${process.env.NEXT_PUBLIC_API_SERVER_URL}/book/random`,
+	);
+
+	if (!response.ok) {
+		return <div>에러가 발생했습니다</div>;
+	}
+
+	const books: BookData[] = await response.json();
+
+	return (
+		<>
+			{books.map((book) => (
+				<BookItem key={book.id} {...book} />
+			))}
+		</>
+	);
+}
+
+async function AllBooks() {
+	const response = await fetch(
+		`${process.env.NEXT_PUBLIC_API_SERVER_URL}/book`,
+	);
+
+	if (!response.ok) {
+		return <div>에러가 발생했습니다</div>;
+	}
+
+	const books: BookData[] = await response.json();
+
+	return (
+		<>
+			{books.map((book) => (
+				<BookItem key={book.id} {...book} />
+			))}
+		</>
+	);
+}
+
+export default async function Page() {
 	return (
 		<div className={style.container}>
 			<section>
 				<h3>지금 추천하는 도서</h3>
-				{books.map((book) => (
-					<BookItem key={book.id} {...book} />
-				))}
+				<RecommendedBooks />
 			</section>
 			<section>
 				<h3>등록된 모든 도서</h3>
-				{books.map((book) => (
-					<BookItem key={book.id} {...book} />
-				))}
+				<AllBooks />
 			</section>
 		</div>
 	);
