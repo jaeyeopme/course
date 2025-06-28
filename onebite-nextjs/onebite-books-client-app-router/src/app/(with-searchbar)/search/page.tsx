4bec207313286c4ba3790d77ba1225a4616c96b5
jaeyeopme <jaeyeopme@gmail.com>
2025-06-28 15:54:58 +0900

    feat: 검색 페이지 데이터 페칭 추가
    - 앱 라우터 페이지에서 query parameter 는 props 로 전달되는 Promise 형태의 searchParams 를 통해 접근 가능

--- onebite-nextjs/onebite-books-client-app-router/src/app/(with-searchbar)/search/page.tsx
+++ onebite-nextjs/onebite-books-client-app-router/src/app/(with-searchbar)/search/page.tsx
@@ -1,5 +1,5 @@
 import BookItem from "@/components/book-item";
-import books from "@/mock/books.json";
+import type { BookData } from "@/types";
 
 export default async function Page({
 	searchParams,
@@ -7,6 +7,16 @@ export default async function Page({
 	searchParams: Promise<{ query?: string }>;
 }) {
 	const { query } = await searchParams;
+
+	const response = await fetch(
+		`${process.env.NEXT_PUBLIC_API_SERVER_URL}/book/search?q=${query || ""}`,
+	);
+
+	if (!response.ok) {
+		return <div>에러가 발생했습니다</div>;
+	}
+
+	const books: BookData[] = await response.json();
 
 	return (
 		<div>
