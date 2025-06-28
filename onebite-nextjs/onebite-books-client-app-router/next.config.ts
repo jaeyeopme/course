ace888e061e3ad5d4f80414f6c8e44d5b31cfc1e
jaeyeopme <jaeyeopme@gmail.com>
2025-06-28 19:55:21 +0900

    chore: 모든 fetch 요청의 URL 로깅 설정

--- onebite-nextjs/onebite-books-client-app-router/next.config.ts
+++ onebite-nextjs/onebite-books-client-app-router/next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+	/* config options here */
+	logging: {
+		fetches: {
+			fullUrl: true, // 모든 fetch 요청의 전체 URL을 로깅
+		},
+	},
 };
 
 export default nextConfig;
