39f692017350c05c331c56c15fdbd6f03462bc52
jaeyeopme <jaeyeopme@gmail.com>
2025-06-30 01:44:15 +0900

    feat: 서버 액션 리뷰 추가 기능 구현

---
+++ onebite-nextjs/onebite-books-client-app-router/src/app/actions/create-review.action.ts
@@ -0,0 +1,30 @@
+"use server";
+
+export async function createReviewAction(formData: FormData) {
+	const bookId = formData.get("bookId");
+	const content = formData.get("content");
+	const author = formData.get("author");
+
+	if (!bookId || !content || !author) return;
+
+	try {
+		const response = await fetch(
+			`${process.env.NEXT_PUBLIC_API_SERVER_URL}/review`,
+			{
+				method: "POST",
+				body: JSON.stringify({
+					bookId,
+					content,
+					author,
+				}),
+			},
+		);
+
+		if (!response.ok) {
+			throw new Error("리뷰 작성에 실패했습니다.");
+		}
+	} catch (error) {
+		console.error("리뷰 작성 중 에러 발생:", error);
+		return;
+	}
+}
--- onebite-nextjs/onebite-books-client-app-router/src/app/book/[id]/page.tsx
+++ onebite-nextjs/onebite-books-client-app-router/src/app/book/[id]/page.tsx
@@ -1,16 +1,11 @@
 import { BookData } from "@/types";
 import style from "./page.module.css";
 import { notFound } from "next/navigation";
+import { createReviewAction } from "@/app/actions/create-review.action";
 
-export default async function Page({
-	params,
-}: {
-	params: Promise<{ id: string | string[] }>;
-}) {
-	const { id } = await params;
-
+async function BookDetail({ bookId }: { bookId: string }) {
 	const response = await fetch(
-		`${process.env.NEXT_PUBLIC_API_SERVER_URL}/book/${id}`,
+		`${process.env.NEXT_PUBLIC_API_SERVER_URL}/book/${bookId}`,
 	);
 
 	if (response.status === 404) notFound();
@@ -25,7 +20,7 @@ export default async function Page({
 	}: BookData = await response.json();
 
 	return (
-		<div className={style.container}>
+		<section>
 			<div
 				className={style.cover_img_container}
 				style={{ backgroundImage: `url('${coverImgUrl}')` }}
@@ -38,6 +33,34 @@ export default async function Page({
 				{author} | {publisher}
 			</div>
 			<div className={style.description}>{description}</div>
+		</section>
+	);
+}
+
+function ReviewEditor({ bookId }: { bookId: string }) {
+	return (
+		<section>
+			<form action={createReviewAction}>
+				<input type="hidden" name="bookId" value={bookId} />
+				<input required name="content" placeholder="리뷰내용" />
+				<input required name="author" placeholder="작성자" />
+				<button type="submit">작성하기</button>
+			</form>
+		</section>
+	);
+}
+
+export default async function Page({
+	params,
+}: {
+	params: Promise<{ id: string }>;
+}) {
+	const { id } = await params;
+
+	return (
+		<div className={style.container}>
+			<BookDetail bookId={id} />
+			<ReviewEditor bookId={id} />
 		</div>
 	);
 }
